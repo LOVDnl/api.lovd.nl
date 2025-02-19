114d52d70d4339c7ee11cf323e2c461c2bb8fa3c
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2025-02-19 12:18:23 +0100

    Make sure the Swagger UI works both online and in our testing env.

--- src/swagger/swagger-initializer.js
+++ src/swagger/swagger-initializer.js
@@ -1,9 +1,15 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
+  if (location.hostname == "localhost") {
+    var path = "localhost/git/api.lovd.nl/src";
+  } else {
+    var path = "api.lovd.nl";
+  }
+
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: window.location.protocol + "//api.lovd.nl/v1/swagger.json",
+    url: window.location.protocol + "//" + path + "/v1/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
